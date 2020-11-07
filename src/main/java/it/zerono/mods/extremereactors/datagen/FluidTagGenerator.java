613d3c53fd926d5037720d73c78a9b408678f9f5
ZeroNoRyouki <zeronoryouki@gmail.com>
2020-11-07 22:03:24 +0100

    Fluid Tag generator for Steam

---
+++ src/main/java/it/zerono/mods/extremereactors/datagen/FluidTagGenerator.java
@@ -0,0 +1,58 @@
+/*
+ *
+ * FluidTagGenerator.java
+ *
+ * This file is part of Extreme Reactors 2 by ZeroNoRyouki, a Minecraft mod.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * DO NOT REMOVE OR EDIT THIS HEADER
+ *
+ */
+
+package it.zerono.mods.extremereactors.datagen;
+
+import it.zerono.mods.extremereactors.ExtremeReactors;
+import it.zerono.mods.extremereactors.gamecontent.Content;
+import it.zerono.mods.extremereactors.gamecontent.ContentTags;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.FluidTagsProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+import javax.annotation.Nullable;
+
+public class FluidTagGenerator
+    extends FluidTagsProvider {
+
+    public FluidTagGenerator(final DataGenerator generator, @Nullable ExistingFileHelper existingFileHelper) {
+        super(generator, ExtremeReactors.MOD_ID, existingFileHelper);
+    }
+
+    //region IDataProvider
+
+    /**
+     * Gets a name for this provider, to use in logging.
+     */
+    @Override
+    public String getName() {
+        return ExtremeReactors.MOD_NAME + " Fluids Tags";
+    }
+
+    //endregion
+    //region FluidTagsProvider
+
+    @Override
+    protected void registerTags() {
+
+        this.getOrCreateBuilder(ContentTags.Fluids.STEAM)
+                .add(Content.Fluids.STEAM_SOURCE.get(), Content.Fluids.STEAM_FLOWING.get());
+    }
+
+    //endregion
+}
