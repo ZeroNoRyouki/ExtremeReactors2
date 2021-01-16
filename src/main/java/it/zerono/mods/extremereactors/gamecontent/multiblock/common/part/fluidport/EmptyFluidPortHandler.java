d5a334550d49764e3d0ef5c9787a20e74f47c8b8
ZeroNoRyouki <zeronoryouki@gmail.com>
2021-01-16 14:00:04 +0100

    Use an empty handler if Mekanism is not around

--- src/main/java/it/zerono/mods/extremereactors/gamecontent/multiblock/common/part/fluidport/EmptyFluidPortHandler.java
+++ src/main/java/it/zerono/mods/extremereactors/gamecontent/multiblock/common/part/fluidport/EmptyFluidPortHandler.java
@@ -1,6 +1,6 @@
 /*
  *
- * IFluidPortHandler.java
+ * EmptyFluidPortHandler.java
  *
  * This file is part of Extreme Reactors 2 by ZeroNoRyouki, a Minecraft mod.
  *
@@ -19,44 +19,27 @@
 package it.zerono.mods.extremereactors.gamecontent.multiblock.common.part.fluidport;
 
 import it.zerono.mods.extremereactors.gamecontent.multiblock.common.AbstractGeneratorMultiblockController;
-import it.zerono.mods.extremereactors.gamecontent.multiblock.common.part.AbstractMultiblockEntity;
-import it.zerono.mods.extremereactors.gamecontent.multiblock.common.part.IIOPortHandler;
+import it.zerono.mods.extremereactors.gamecontent.multiblock.common.part.EmptyIOPortHandler;
 import it.zerono.mods.extremereactors.gamecontent.multiblock.common.variant.IMultiblockGeneratorVariant;
-import it.zerono.mods.zerocore.lib.block.multiblock.IMultiblockVariantProvider;
-import it.zerono.mods.zerocore.lib.compat.Mods;
-import it.zerono.mods.zerocore.lib.data.IIoEntity;
-import it.zerono.mods.zerocore.lib.data.IoMode;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.capability.IFluidHandler;
 
-public interface IFluidPortHandler<Controller extends AbstractGeneratorMultiblockController<Controller, V>,
-            V extends IMultiblockGeneratorVariant>
-        extends IIOPortHandler {
+public class EmptyFluidPortHandler<Controller extends AbstractGeneratorMultiblockController<Controller, V>,
+                                    V extends IMultiblockGeneratorVariant>
+    extends EmptyIOPortHandler
+    implements IFluidPortHandler<Controller, V> {
 
-    static <Controller extends AbstractGeneratorMultiblockController<Controller, V>, V extends IMultiblockGeneratorVariant,
-            T extends AbstractMultiblockEntity<Controller> & IMultiblockVariantProvider<? extends IMultiblockGeneratorVariant> & IIoEntity>
-    IFluidPortHandler<Controller, V> create(final FluidPortType type, final IoMode mode, final T part) {
-
-        switch (type) {
-
-            case Forge:
-                return new FluidPortHandlerForge<>(part, mode);
-
-            case Mekanism:
-                return Mods.MEKANISM.map(() -> (IFluidPortHandler<Controller, V>)new FluidPortHandlerMekanism<>(part, mode))
-                        .orElseGet(EmptyFluidPortHandler::new);
-
-            default:
-                throw new IllegalArgumentException("Unsupported Fluid Port: " + type);
-        }
-    }
+    //region IFluidPortHandler
 
     /**
      * Get the {@link FluidPortType} supported by this IFluidPortHandler
      *
      * @return the supported {@link FluidPortType}
      */
-    FluidPortType getFluidPortType();
+    @Override
+    public FluidPortType getFluidPortType() {
+        return FluidPortType.Forge;
+    }
 
     /**
      * If this is a Active Fluid Port in output mode, send fluid to the connected consumer (if there is one)
@@ -64,14 +47,21 @@ public interface IFluidPortHandler<Controller extends AbstractGeneratorMultibloc
      * @param stack FluidStack representing the Fluid and maximum amount of fluid to be sent out.
      * @return the amount of fluid accepted by the consumer
      */
-    int outputFluid(FluidStack stack);
-
+    @Override
+    public int outputFluid(FluidStack stack) {
+        return 0;
+    }
 
     /**
      * If this is a Active Fluid Port in input mode, try to get fluids from the connected consumer (if there is one)
      *
      * @param destination the destination IFluidHandler that will receive the fluid
-     * @param maxAmount the maximum amount of fluid the acquire
+     * @param maxAmount   the maximum amount of fluid the acquire
      */
-    int inputFluid(IFluidHandler destination, int maxAmount);
+    @Override
+    public int inputFluid(IFluidHandler destination, int maxAmount) {
+        return 0;
+    }
+
+    //endregion
 }
