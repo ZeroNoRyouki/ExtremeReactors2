3b79f9a151b570afdcaa456714e9220ee8767334
ZeroNoRyouki <zeronoryouki@gmail.com>
2021-12-01 19:47:35 +0100

    Fixed build cmds

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 @echo off
 cd ..
 call gradlew.bat :ExtremeReactors2-1.17:build
-cd ExtremeReactors2-master
+cd ExtremeReactors2-1.17
--- publish.cmd
+++ publish.cmd
@@ -1,4 +1,4 @@
 @echo off
 cd ..
 call gradlew.bat :ExtremeReactors2-1.17:publish
-cd ExtremeReactors2-master
+cd ExtremeReactors2-1.17
