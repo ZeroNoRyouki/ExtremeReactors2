b8aa9c25fa5576a22d9cc11e4954ec438ad5e8d2
ZeroNoRyouki <zeronoryouki@gmail.com>
2022-06-11 15:43:25 +0200

    Bumped version to 2.0.46

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 @echo off
 cd ..
-call gradlew.bat :ExtremeReactors2-1.18:build
-cd ExtremeReactors2-1.18
+call gradlew.bat :ExtremeReactors2-1.19:build
+cd ExtremeReactors2-1.19
--- publish.cmd
+++ publish.cmd
@@ -1,4 +1,4 @@
 @echo off
 cd ..
-call gradlew.bat :ExtremeReactors2-1.18:publish
-cd ExtremeReactors2-1.18
+call gradlew.bat :ExtremeReactors2-1.19:publish
+cd ExtremeReactors2-1.19
