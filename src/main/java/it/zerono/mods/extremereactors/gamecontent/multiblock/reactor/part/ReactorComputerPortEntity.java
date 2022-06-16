315f67b418a045dec4672ab8cdd62478965e6383
ZeroNoRyouki <zeronoryouki@gmail.com>
2022-06-16 21:02:37 +0200

    Added back CC-Tweaked support Updated CC-Tweaked to version 1.19-1.100.7

--- src/main/java/it/zerono/mods/extremereactors/gamecontent/multiblock/reactor/part/ReactorComputerPortEntity.java
+++ src/main/java/it/zerono/mods/extremereactors/gamecontent/multiblock/reactor/part/ReactorComputerPortEntity.java
@@ -18,20 +18,24 @@
 
 package it.zerono.mods.extremereactors.gamecontent.multiblock.reactor.part;
 
-//import dan200.computercraft.api.peripheral.IPeripheral;
-
+import dan200.computercraft.api.peripheral.IPeripheral;
 import it.zerono.mods.extremereactors.gamecontent.Content;
+import it.zerono.mods.extremereactors.gamecontent.multiblock.reactor.MultiblockReactor;
 import it.zerono.mods.extremereactors.gamecontent.multiblock.reactor.computer.ReactorComputerPeripheral;
+import it.zerono.mods.zerocore.lib.compat.Mods;
+import it.zerono.mods.zerocore.lib.compat.computer.ConnectorComputerCraft;
+import it.zerono.mods.zerocore.lib.compat.computer.MultiblockComputerPeripheral;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.level.block.state.BlockState;
-//import net.minecraftforge.common.capabilities.Capability;
-//import net.minecraftforge.common.capabilities.CapabilityManager;
-//import net.minecraftforge.common.capabilities.CapabilityToken;
-//import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityManager;
+import net.minecraftforge.common.capabilities.CapabilityToken;
+import net.minecraftforge.common.util.LazyOptional;
 
-//import javax.annotation.Nonnull;
-//import javax.annotation.Nullable;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 public class ReactorComputerPortEntity
         extends AbstractReactorEntity {
@@ -40,9 +44,9 @@ public class ReactorComputerPortEntity
 
         super(Content.TileEntityTypes.REACTOR_COMPUTERPORT.get(), position, blockState);
 
-//        this._ccConnector = Mods.COMPUTERCRAFT
-//                .map(() -> LazyOptional.of(() -> ConnectorComputerCraft.create("BigReactors-Reactor", this.getPeripheral())))
-//                .orElse(null);
+        this._ccConnector = Mods.COMPUTERCRAFT
+                .map(() -> LazyOptional.of(() -> ConnectorComputerCraft.create("BigReactors-Reactor", this.getPeripheral())))
+                .orElse(null);
         // TODO OC
     }
 
@@ -58,10 +62,10 @@ public class ReactorComputerPortEntity
 
         super.syncDataFrom(data, syncReason);
 
-////        this.executeOnComputerCraftConnector(c -> c.syncDataFrom(data, syncReason));
-//        if (null != this._ccConnector) {
-//            this._ccConnector.ifPresent(c -> c.syncDataFrom(data, syncReason));
-//        }
+//        this.executeOnComputerCraftConnector(c -> c.syncDataFrom(data, syncReason));
+        if (null != this._ccConnector) {
+            this._ccConnector.ifPresent(c -> c.syncDataFrom(data, syncReason));
+        }
     }
 
     /**
@@ -75,10 +79,10 @@ public class ReactorComputerPortEntity
 
         super.syncDataTo(data, syncReason);
 
-////        this.executeOnComputerCraftConnector(c -> c.syncDataTo(data, syncReason));
-//        if (null != this._ccConnector) {
-//            this._ccConnector.ifPresent(c -> c.syncDataTo(data, syncReason));
-//        }
+//        this.executeOnComputerCraftConnector(c -> c.syncDataTo(data, syncReason));
+        if (null != this._ccConnector) {
+            this._ccConnector.ifPresent(c -> c.syncDataTo(data, syncReason));
+        }
 
         return data;
     }
@@ -86,41 +90,40 @@ public class ReactorComputerPortEntity
     //endregion
     //region AbstractReactorEntity
 
-//    @Override
-//    public void onAttached(MultiblockReactor newController) {
-//
-//        super.onAttached(newController);
-////        this.executeOnComputerCraftConnector(Connector::onAttachedToController);
-//
-//        if (null != this._ccConnector) {
-//            //noinspection Convert2MethodRef
-//            this._ccConnector.ifPresent(c -> c.onAttachedToController());
-//        }
-//    }
+    @Override
+    public void onAttached(MultiblockReactor newController) {
 
-//    @Override
-//    public void onDetached(MultiblockReactor oldController) {
-//
-//        super.onDetached(oldController);
-////        this.executeOnComputerCraftConnector(Connector::onDetachedFromController);
-//        if (null != this._ccConnector) {
-//            //noinspection Convert2MethodRef
-//            this._ccConnector.ifPresent(c -> c.onDetachedFromController());
-//        }
-//    }
+        super.onAttached(newController);
+//        this.executeOnComputerCraftConnector(Connector::onAttachedToController);
+        if (null != this._ccConnector) {
+            //noinspection Convert2MethodRef
+            this._ccConnector.ifPresent(c -> c.onAttachedToController());
+        }
+    }
+
+    @Override
+    public void onDetached(MultiblockReactor oldController) {
+
+        super.onDetached(oldController);
+//        this.executeOnComputerCraftConnector(Connector::onDetachedFromController);
+        if (null != this._ccConnector) {
+            //noinspection Convert2MethodRef
+            this._ccConnector.ifPresent(c -> c.onDetachedFromController());
+        }
+    }
 
     //endregion
     //region TileEntity
 
-//    @Override
-//    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> capability, @Nullable Direction side) {
-//
-//        if (!this.isRemoved() && (null != this._ccConnector) && CAPABILITY_CC_PERIPHERAL == capability) {
-//                return this._ccConnector.cast();
-//        }
-//
-//        return super.getCapability(capability, side);
-//    }
+    @Override
+    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> capability, @Nullable Direction side) {
+
+        if (!this.isRemoved() && (null != this._ccConnector) && CAPABILITY_CC_PERIPHERAL == capability) {
+                return this._ccConnector.cast();
+        }
+
+        return super.getCapability(capability, side);
+    }
 
     //endregion
     //region client render support
@@ -149,10 +152,10 @@ public class ReactorComputerPortEntity
 //        }
 //    }
 
-//    @SuppressWarnings("FieldMayBeFinal")
-//    public static Capability<IPeripheral> CAPABILITY_CC_PERIPHERAL = CapabilityManager.get(new CapabilityToken<>(){});
-//
-//    private final LazyOptional<ConnectorComputerCraft<MultiblockComputerPeripheral<MultiblockReactor, ReactorComputerPortEntity>>> _ccConnector;
+    @SuppressWarnings("FieldMayBeFinal")
+    public static Capability<IPeripheral> CAPABILITY_CC_PERIPHERAL = CapabilityManager.get(new CapabilityToken<>(){});
+
+    private final LazyOptional<ConnectorComputerCraft<MultiblockComputerPeripheral<MultiblockReactor, ReactorComputerPortEntity>>> _ccConnector;
 
     private ReactorComputerPeripheral _peripheral;
 
